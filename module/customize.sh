ffb792ba88ee5fe159091f02893d26c035886102
qnnnnez <qnnnnez@live.com>
2024-10-24 20:35:33 +0800

    module: do not set -xe as customize.sh is sourced

--- module/customize.sh
+++ module/customize.sh
@@ -1,5 +1,3 @@
-set -xe
-
 ui_print "Disabling .esim.ability.EuiccGoogleReceiver broadcast receiver for com.android.phone ..."
 pm disable com.android.phone/.esim.ability.EuiccGoogleReceiver
 ui_print "Done."
